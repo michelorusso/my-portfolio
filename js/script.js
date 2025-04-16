c7d6fe1e2bcb06bc485f37cd306998805a260026
Michele Lorusso <miky-l@hotmail.it>
2025-04-16 11:24:34 +0200

    feat: Recommendation e footer section

--- js/script.js
+++ js/script.js
@@ -15,3 +15,20 @@ var swiper = new Swiper(".mySwiper", {
     el: ".swiper-pagination",
   },
 });
+
+function addRecommendation() {
+  const container = document.getElementById("all_recommendations");
+  const textarea = document.getElementById("new_recommendation");
+  const text = textarea.value.trim();
+
+  if (text !== "") {
+    const newRec = document.createElement("div");
+    newRec.className = "recommendation";
+    newRec.innerHTML = `<span>&#8220;</span>${text}<span>&#8221;</span>`;
+    container.appendChild(newRec);
+    textarea.value = "";
+    alert("Thanks for your recommendation!");
+  } else {
+    alert("Please write a message before submitting.");
+  }
+}
